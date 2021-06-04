fd7a77b92695bd939703aae3dd6bc844f4dfa7cb
Rostisvav M <70744775+Rostislav9821@users.noreply.github.com>
2021-06-04 06:17:09 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- config: utf-8 -*-
+
+from tkinter import *
+
+
+def about():
+    a = Toplevel()
+    a.geometry('200x150')
+    a['bg'] = 'grey'
+    a.overrideredirect(True)
+    Label(a, text="About this")\
+        .pack(expand=1)
+    a.after(5000, lambda: a.destroy())
+
+
+root = Tk()
+root.title("Главное окно")
+
+root.title("Главное окно")
+Button(text="Button", width=20).pack()
+Label(text="Label", width=20, height=3)\
+    .pack()
+Button(text="About", width=20, command=about)\
+    .pack()
+
+root.mainloop()
---
+++ 7.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- config: utf-8 -*-
+
+from tkinter import *
+root = Tk()
+
+mainmenu = Menu(root)
+root.config(menu=mainmenu)
+
+filemenu = Menu(mainmenu, tearoff=0)
+filemenu.add_command(label="Открыть...")
+filemenu.add_command(label="Новый")
+filemenu.add_command(label="Сохранить...")
+filemenu.add_command(label="Выход")
+
+helpmenu = Menu(mainmenu, tearoff=0)
+helpmenu2 = Menu(helpmenu, tearoff=0)
+helpmenu2.add_command(label="Локальная справка")
+helpmenu2.add_command(label="На сайте")
+helpmenu.add_cascade(label="Помощь", menu=helpmenu2)
+helpmenu.add_command(label="О программе")
+
+mainmenu.add_cascade(label="Файл", menu=filemenu)
+mainmenu.add_cascade(label="Справка", menu=helpmenu)
+
+root.mainloop()
